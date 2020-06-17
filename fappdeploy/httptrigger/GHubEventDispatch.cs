b604ce1b45ea99729f6def2896f9fba41454d522
Ashish Kumar (ASHKUMA) <ashishonce@gmail.com>
2020-06-17 17:50:06 +0530

    trying to fix a bug

--- fappdeploy/httptrigger/GHubEventDispatch.cs
+++ fappdeploy/httptrigger/GHubEventDispatch.cs
@@ -39,7 +39,7 @@ public static class GridEventHandler{
         dynamic requestObject = JsonConvert.DeserializeObject(requestBody);
         var webhook_res = string.Empty;
         var current_event = requestObject[0]["eventType"];
-        // log.LogInformation(requestObject);
+        log.LogInformation(current_event);
 
         if (current_event == "Microsoft.EventGrid.SubscriptionValidationEvent" ){
             if (requestObject != null && requestObject[0]["data"] != null){
@@ -52,7 +52,7 @@ public static class GridEventHandler{
         }
         
 
-        if (current_event.Contains("Microsoft.MachineLearningServices"))
+        if (current_event.Contains("Microsoft.MachineLearningServices", OrdinalIgnoreCase))
         {
             using (var httpClient = new HttpClient())
             {
